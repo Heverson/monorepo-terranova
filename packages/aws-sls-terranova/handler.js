351fa11b8da7f29d9882f62099bb4101b9259b43
Heverson <heversondamasceno@gmail.com>
2021-11-01 11:44:46 -0300

    chore: add new repo sls

---
+++ packages/aws-sls-terranova/handler.js
@@ -0,0 +1,135 @@
+"use strict";
+const fetch = require("node-fetch");
+
+const getInfo = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v2.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const getBanners = async (event) => {
+  const result = await fetch(
+    "https://www.shoppingterranova.com.py/api/?action=banners"
+  );
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const getBrands = async (event) => {
+  const result = await fetch(
+    "https://www.shoppingterranova.com.py/api/?action=brands"
+  );
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const getCategories = async (event) => {
+  const result = await fetch(
+    "https://www.shoppingterranova.com.py/api/?action=categories"
+  );
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const getCategoryProducts = async (event) => {
+  const { type } = event.pathParameters;
+  const result = await fetch(
+    `https://www.shoppingterranova.com.py/api/?action=products&type=${type}`
+  );
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const getProducts = async (event) => {
+  const result = await fetch(
+    "https://www.shoppingterranova.com.py/api/?action=categories"
+  );
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+const createSession = async (event) => {
+  const { username, password } = JSON.parse(event.body);
+  const url = `https://www.shoppingterranova.com.py/api/?action=session&login=${username}&password=${password}`;
+  const result = await fetch(url);
+  const data = await result.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        result: data,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports = {
+  getInfo,
+  getBanners,
+  getBrands,
+  getCategories,
+  getCategoryProducts,
+  getProducts,
+  createSession,
+};
